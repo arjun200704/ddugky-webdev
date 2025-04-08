374157d64a4f801028f9cccc7b31e6133123b3fa
Arjun Meda <arjun.m042007@gmail.com>
2025-04-08 11:49:08 +0530

    learne if else

--- JS-Fundamentals/01_datatypes_variables.js
+++ JS-Fundamentals/01_datatypes_variables.js
@@ -1,10 +1,24 @@
+////////////////////////////////////
+// 01. data types 
+///////////////////////////////////
+ /*// string enclosed within single quotes 
+ let fruit = 'apple';
+ console.log(fruit) 
+ 
+ //  string enclosed within double quotes
 
+ let country = "USA";
+ console.log(country);
+
+ // string enclosed within backticks
+ let result = `pass`;
+ console.log(result); */
 
 ///////////////////////////////////
 // 02. Variables
 ///////////////////////////////////
 
-// Identifiers/Name & Literals/Value
+/*// Identifiers/Name & Literals/Value
 var firstName = "arjun";
 var lastName = "meda";
 var age = 18;
@@ -17,7 +31,7 @@ console.log(firstName, lastName);
 console.log("Good Morning", firstName, lastName, "!");
 console.log("Hi, My name is", firstName, "and my age is", age, ".");
 console.log("My wife is", wifeName);
-console.log("My thrid Name is", thirdName);
+console.log("My thrid Name is", thirdName); */
 
 //////////////////////////
 // integer variable //
---
+++ JS-Fundamentals/03 _if_else.js
@@ -0,0 +1,40 @@
+ /*
+ // MARKS SHEET
+ var marks = 90;
+ var grade ;
+ var conditionA = marks >=90;
+ var conditionB = marks >=70 && marks <90;
+ var conditionC = marks >=50 && marks <70;
+ var conditionD = marks >=33 && marks <50;
+ var conditionF = marks <33;
+
+ if (conditionA) grade = "A";          
+ else if (conditionB) grade = "B";     
+ else if (conditionC) grade = "C";
+ else if (conditionD) grade = "D";
+ else if (conditionF) grade =  "F"
+ console.log("your marks is", marks)
+ console.log("your grade is", grade )
+ */
+ 
+ // PASS OR FALL && NUMBER
+
+ var marks =  78;
+ var grade ;
+ var conditionA = marks >=85;
+ var conditionB = marks >= 65 && marks <85;  
+ var conditionC = marks >=45 && marks <65;
+ var conditionD = marks >= 33 && marks <45;
+ var conditionF = marks <33 
+ if (conditionA) grade = "A";
+ else if (conditionB) grade = "B";
+ else if (conditionC) grade = "C";
+ else if (conditionD) grade = "D";
+ else if (conditionF)grade = "F";
+ console.log("your marks is", marks)
+ console.log("your grate is", grade)
+
+  
+
+
+
