7a4a0af445d6c5ba0a13ee66258973fcc9af0784
Arjun Meda <arjun.m042007@gmail.com>
2025-04-09 13:27:16 +0530

    loop

---
+++ JS-Fundamentals/04_while_loop.js
@@ -0,0 +1,14 @@
+/*
+let tableOf = 11;
+let num = 1;
+while (num <=10 ){
+  console.log(tableOf,"x",num,"=", tableOf * num);
+  num++;
+}
+  */
+let squ = 8;
+let num = 1;
+while (num <=20){
+  console.log(squ,"x",num,"=", squ * num);
+  num++;
+}
