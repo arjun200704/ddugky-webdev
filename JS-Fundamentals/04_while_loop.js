4c6bf6b62ee5ddc2e81f0580af19ed20cd0f40bf
Arjun Meda <arjun.m042007@gmail.com>
2025-04-10 11:41:36 +0530

    loop prectice

--- JS-Fundamentals/04_while_loop.js
+++ JS-Fundamentals/04_while_loop.js
@@ -6,9 +6,40 @@ while (num <=10 ){
   num++;
 }
   */
+ /*
 let squ = 8;
 let num = 1;
 while (num <=20){
   console.log(squ,"x",num,"=", squ * num);
   num++;
 }
+  */
+ ///////////////////////////////
+ //PRINT ALL EVEN NUM/////////
+ ////////////////////////////
+/*
+ let num =1 ;
+ let max = 50;
+ let count = 0;
+ while (num <= max ) {
+  if (num % 2 == 0)console.log(num);
+  count++;
+  num++;
+ }
+  */
+ 
+//////////////////////////
+/// PRINT ALL ODD NUM////
+/////////////////////////
+/*
+ 
+let num =1 ;
+let max = 50;
+while (num <= max ) {
+ if (num % 2 == 1)console.log(num);
+ num++;
+}
+ */
+///////////////////////
+///print table//////
+///////////////////////
